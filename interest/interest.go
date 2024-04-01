3d924279bc12d1b187212bf825eeecfb188c2410
Kasjan Chilarski <keistzen@gmail.com>
2024-04-01 16:46:25 +0200

    add float interest exercise

---
+++ interest/interest.go
@@ -0,0 +1,40 @@
+package interest
+
+// (-Inf:0): -1.03213
+// [0:1000): 1.005
+// [1000:5000): 1.01621
+// [5000:+Inf): 1.02475
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance < 1000:
+		return 0.5
+	case balance < 5000:
+		return 1.621
+	default:
+		return 2.475
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * float64(InterestRate(balance)) / 100
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return Interest(balance) + balance
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	years := 0
+	for balance < targetBalance {
+		years++
+		balance = AnnualBalanceUpdate(balance)
+	}
+	return years
+}
